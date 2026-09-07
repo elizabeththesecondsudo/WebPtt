a47b766c99e026c3f3dd4331a6e6900fdfb4f8e0
FlomkoMoon <vakninofek3@gmail.com>
2026-09-07 20:24:04 +0300

    feat: added mixing effect

--- backend/src/Audio/Gain.cpp
+++ backend/src/Audio/Gain.cpp
@@ -6,11 +6,11 @@
 namespace WebPtt::Audio {
 namespace {
 
-constexpr float kDecibelAmplitudeDivisor = 20.0F;
-constexpr float kDecibelBase = 10.0F;
+constexpr auto kDecibelAmplitudeDivisor = 20.0F;
+constexpr auto kDecibelBase = 10.0F;
 
-constexpr float kMinSample = -1.0F;
-constexpr float kMaxSample = 1.0F;
+constexpr auto kMinSample = -1.0F;
+constexpr auto kMaxSample = 1.0F;
 
 constexpr float db_to_gain(float decibels) {
     return std::pow(kDecibelBase, decibels / kDecibelAmplitudeDivisor);
@@ -22,8 +22,8 @@ Gain::Gain(float decibels)
     : gain_factor_(db_to_gain(decibels)) {}
 
 void Gain::process(std::span<float> samples) const {
-    for (float& sample : samples) {
-        const float amplified_sample = sample * gain_factor_;
+    for (auto& sample : samples) {
+        const auto amplified_sample = sample * gain_factor_;
 
         sample = std::clamp(amplified_sample, kMinSample, kMaxSample);
     }
